3f28989ab402ccf85d9d49ea4ea4587fe4535d4d
Spencer Lyon <spencerlyon2@gmail.com>
2020-05-28 17:03:29 -0400

    ENH: use rapidoc for rest-api page

--- pages/rest-api.jsx
+++ pages/rest-api.jsx
@@ -5,7 +5,7 @@ import PageBanner from "../components/Common/PageBanner";
 // import CtaAreaTwo from "../components/Common/CtaAreaTwo";
 // import WIP from "../components/Common/WIP";
 import Footer from "../components/Layouts/Footer";
-import { RedocStandalone } from "redoc";
+import "rapidoc";
 
 class ServiceDetails extends Component {
   render() {
@@ -13,7 +13,17 @@ class ServiceDetails extends Component {
       <React.Fragment>
         <NavbarThree />
         <PageBanner pageTitle="REST API" />
-        <RedocStandalone specUrl="https://clean-swagger-inunbrtacq-uk.a.run.app" />
+        {/* <RedocStandalone specUr`l="https://clean-swagger-inunbrtacq-uk.a.run.app" /> */}
+        <rapi-doc
+          spec-url="https://api.covid.valorum.ai/swagger.json"
+          render-style="view"
+          schema-style="table"
+          show-header="false"
+          layout="column"
+          schema-description-expanded="true"
+          show-info="true"
+          allow-server-selection="false"
+        />
 
         {/* <WIP imgname="undraw_medical_research_qg4d.svg" /> */}
         {/* <ServiceDetailsContent /> */}
