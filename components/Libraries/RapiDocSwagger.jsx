4eeab1bc25210376e7659ffdc8de93bcb43593b8
Spencer Lyon <spencerlyon2@gmail.com>
2020-06-05 00:09:30 -0400

    ENH: wrap rapi-doc in container

--- components/Libraries/RapiDocSwagger.jsx
+++ components/Libraries/RapiDocSwagger.jsx
@@ -3,18 +3,22 @@ import "rapidoc";
 
 function RapiDoc() {
   return (
-    <React.Fragment>
-      <rapi-doc
-        spec-url="https://api.covid.valorum.ai/swagger.json"
-        render-style="view"
-        schema-style="table"
-        show-header="false"
-        layout="column"
-        schema-description-expanded="true"
-        show-info="true"
-        allow-server-selection="false"
-      />
-    </React.Fragment>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12">
+          <rapi-doc
+            spec-url="https://api.covid.valorum.ai/swagger.json"
+            render-style="view"
+            schema-style="table"
+            show-header="false"
+            layout="column"
+            schema-description-expanded="true"
+            show-info="true"
+            allow-server-selection="false"
+          />
+        </div>
+      </div>
+    </div>
   );
 }
 
