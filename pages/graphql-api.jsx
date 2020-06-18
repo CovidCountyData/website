ee27688eb22552a97cb4a6e7526494e479eded79
Spencer Lyon <spencerlyon2@gmail.com>
2020-06-18 15:34:00 -0400

    ENH: put graphiql in container div

--- pages/graphql-api.jsx
+++ pages/graphql-api.jsx
@@ -1,29 +1,31 @@
 import React, { Component } from "react";
 import NavbarThree from "../components/Layouts/Navbar";
 import PageBanner from "../components/Common/PageBanner";
-// import ServiceDetailsContent from "../components/Libraries/ServiceDetailsContent";
-// import CtaAreaTwo from "../components/Common/CtaAreaTwo";
 import Footer from "../components/Layouts/Footer";
-import WIP from "../components/Common/WIP";
 
-class ServiceDetails extends Component {
+class GraphQL extends Component {
   render() {
     return (
       <React.Fragment>
         <NavbarThree />
         <PageBanner pageTitle="GraphQL API" />
-        {/* <WIP imgname="undraw_social_distancing_2g0u.svg" /> */}
-        <iframe
-          src="https://api.covid.valorum.ai/graphiql"
-          height="800px"
-          width="100%"
-        />
-        {/* <ServiceDetailsContent /> */}
-        {/* <CtaAreaTwo /> */}
+
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <iframe
+                src="https://api.covid.valorum.ai/graphiql"
+                height="800px"
+                width="100%"
+              />
+            </div>
+          </div>
+        </div>
+
         <Footer />
       </React.Fragment>
     );
   }
 }
 
-export default ServiceDetails;
+export default GraphQL;
--- pages/julia.jsx
+++ pages/julia.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import NavbarThree from "../components/Layouts/Navbar";
 import PageBanner from "../components/Common/PageBanner";
-// import ServiceDetailsContent from "../components/Libraries/ServiceDetailsContent";
-// import CtaAreaTwo from "../components/Common/CtaAreaTwo";
 import Footer from "../components/Layouts/Footer";
 import JlDocs from "../components/Libraries/JlDocs";
 
