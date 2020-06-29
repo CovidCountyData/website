c14a051c7652d108a8c0c2bfac22d66c5cafb0a5
Spencer Lyon <spencerlyon2@gmail.com>
2020-06-29 16:38:25 -0400

    ENH: add registration to index

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,20 +2,8 @@ import React, { Component } from "react";
 import NavbarThree from "../components/Layouts/Navbar";
 import MainBanner from "../components/Home/MainBanner";
 import FeaturedSources from "../components/Home/FeaturedSources";
-// import CaseStudies from "../components/Home/CaseStudies";
-// import FeedbackTwo from "../components/Common/FeedbackTwo";
-// import BestServices from "../components/Home/BestServices";
-// import Partner from "../components/Common/Partner";
-// import Feedback from "../components/Common/Feedback";
-// import Portfolio from "../components/Home/Portfolio";
-// import TeamStyleFour from "../components/Common/TeamStyleFour";
 import FunFactsTwo from "../components/Common/FunFactsTwo";
-// import WorkProcess from "../components/Home/WorkProcess";
-// import PricingStyleThree from "../components/Pricing/PricingStyleThree";
-// import LatestNews from "../components/Common/LatestNews";
-// import Newsletter from "../components/Common/Newsletter";
-// import CtaAreaTwo from "../components/Common/CtaAreaTwo";
-import Footer from "../components/Layouts/Footer";
+import RegisterFormContainer from "../components/Register/RegisterFormContainer";
 
 class Home extends Component {
   render() {
@@ -24,29 +12,8 @@ class Home extends Component {
         <NavbarThree />
         <MainBanner />
         <FeaturedSources />
-        {/* <CaseStudies /> */}
-        {/* <FeedbackTwo /> */}
-        {/* <BestServices /> */}
-        {/* <Partner /> */}
-        {/* <Feedback /> */}
-        {/* <Portfolio /> */}
-        {/* <TeamStyleFour /> */}
         <FunFactsTwo />
-        {/* <WorkProcess /> */}
-        {/* <PricingStyleThree /> */}
-
-        {/*
-        <div className="pt-100">
-          <Newsletter />
-        </div>
-
-        <div className="ptb-100">
-          <LatestNews />
-        </div>
-
-        <CtaAreaTwo />
-        */}
-        {/*<Footer />*/}
+        <RegisterFormContainer />
       </React.Fragment>
     );
   }
