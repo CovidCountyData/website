36c79e7c6f4b81d09f24559819f655c354a88a25
Spencer Lyon <spencerlyon2@gmail.com>
2020-08-02 06:27:58 -0700

    ENH: add anchor to register form so we can navigate directly to it

--- components/Register/RegisterForm.jsx
+++ components/Register/RegisterForm.jsx
@@ -13,34 +13,37 @@ const RegisterForm = (props) => {
   });
 
   return (
+
     <div className="contact-form pb-50">
       <div className="container">
         <div className="row d-flex justify-content-center align-items-center">
           <div className="col-auto">
             <h2>{title}</h2>
           </div>
-          <div className="col-auto">
-            <form id="registerForm" onSubmit={submit} className="form-inline">
-              <div className="form-group">
-                <input
-                  type="text"
-                  name="email"
-                  ref={register}
-                  id="name"
-                  className="form-control"
-                  placeholder="Your Email"
-                  required
-                />
-              </div>
-              <button
-                type="submit"
-                className="btn btn-primary"
-                onClick={(x) => console.log("submitting", x)}
-              >
-                Register
+          <a name="register">
+            <div className="col-auto">
+              <form id="registerForm" onSubmit={submit} className="form-inline">
+                <div className="form-group">
+                  <input
+                    type="text"
+                    name="email"
+                    ref={register}
+                    id="name"
+                    className="form-control"
+                    placeholder="Your Email"
+                    required
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-primary"
+                  onClick={(x) => console.log("submitting", x)}
+                >
+                  Register
               </button>
-            </form>
-          </div>
+              </form>
+            </div>
+          </a>
         </div>
       </div>
     </div>
