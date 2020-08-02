3613110585e1e5b6031ca1cb7740d2aeceea0231
Spencer Lyon <spencerlyon2@gmail.com>
2020-08-02 06:27:29 -0700

    ENH: add docs for graphql page, in addition to the graphiql embed

--- components/Libraries/GraphQLApi.jsx
+++ components/Libraries/GraphQLApi.jsx
@@ -1,18 +1,22 @@
 import React from "react";
+import GraphQLDocs from "./GraphQLDocs"
 
 const GraphQLApi = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-12">
-          <iframe
-            src="https://api.covid.valorum.ai/graphiql"
-            height="800px"
-            width="100%"
-          />
+    <React.Fragment>
+      <GraphQLDocs />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <iframe
+              src="https://api.covid.valorum.ai/graphiql"
+              height="800px"
+              width="100%"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
