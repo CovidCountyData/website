9e4b383b5a211de41ff37339d6dd16135efd9ec2
Trevor Lyon <trevor.lyon@icloud.com>
2020-08-20 16:28:50 -0300

    FIX: Change order of datasets

--- components/DataDocs/SwaggerDocs.jsx
+++ components/DataDocs/SwaggerDocs.jsx
@@ -7,6 +7,8 @@ import ReactMarkdown from "react-markdown";
 import useRequest from "../../utils/useRequest";
 
 function CustomToggle({ children, eventKey }) {
+
+
   const decoratedOnClick = useAccordionToggle(eventKey, () =>
     console.log("totally custom!")
   );
@@ -25,7 +27,23 @@ const SwaggerDocs = () => {
   const { data, error } = useRequest(
     "https://clean-swagger-inunbrtacq-uk.a.run.app"
   );
-  console.log("Swagger:\n", data);
+  // console.log("Swagger:\n", data);
+  const order = ["covid_us",
+    "covid_historical",
+    "economics",
+    "demographics",
+    "economic_snapshots",
+    "mobility_devices",
+    "mobility_locations",
+    "hhs",
+    "us_counties",
+    "us_states",
+    "npi_us",
+    "covid_global",
+    "covidtrackingproject",
+    "nytimes_covid",
+    "usafacts_covid"]
+
   return (
     <div className="swagger-docs container pb-100">
       <h2>Datasets</h2>
@@ -35,18 +53,19 @@ const SwaggerDocs = () => {
         <div className="col-6">Description</div>
       </div>
       {data &&
-        Object.keys(data.paths).map((key, i) => {
-          const obj = data.paths[key];
-          const definition = data.definitions[key.substring(1)];
+        Object.keys(data.definitions).map((k, i) => {
+          const key = order[i]
+          const obj = data.paths["/" + key];
+          const definition = data.definitions[key];
           if (!definition) {
             return (<div></div>)
           }
           console.log(key, " description: ", definition)
-          const properties = definition['properties']
+          // const properties = definition['properties']
           return (
             <Accordion className="path-desc">
               <CustomToggle eventKey={i + 1}>
-                <div className="col-3">{data['definitions'][key.substring(1)]['name']}</div>
+                <div className="col-3">{definition['name']}</div>
                 <div className="path col-3">{key}</div>
                 <div className="summary col-6">{obj.get.summary}</div>
               </CustomToggle>
