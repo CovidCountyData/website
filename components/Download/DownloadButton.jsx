53ea5c246bcbf5d1de91f43367b34190dc6a8a6d
Trevor Lyon <trevor.lyon@icloud.com>
2020-08-26 12:25:09 -0300

    generate new url for current date

--- components/Download/DownloadButton.jsx
+++ components/Download/DownloadButton.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactTooltip from "react-tooltip";
 import { useMixpanel } from "../Common/mixpanel";
-
+import moment from 'moment'
 const DownloadButton = (props) => {
   const { name, url, description } = props;
   const tipName = name + "-tooltip";
   const mixpanel = useMixpanel();
+  const [useURL, setUseURL] = React.useState(url)
+  useEffect(() => {
+    // Check for existance of current day link
+    // /all_current_date_2020-08-11T20:00
+    const split = url.split('/')
+    const newEnd = split[split.length - 1].replace('latest', moment().format("YYYY-MM-DDThh:00"))
+
+    split.pop()
+    split.push(newEnd)
+    console.log("new URL: ", split.join('/'))
+  }, []) //run only once
 
   const onClick = () => mixpanel.track(name + " download");
 
