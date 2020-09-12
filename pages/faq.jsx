86efcc6cdc02dab7d418036cfd7e12abd69f4601
Spencer Lyon <spencerlyon2@gmail.com>
2020-09-12 20:39:00 +0000

    ENH: send faq requests to contacts collection in firebase

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -7,11 +7,29 @@ import firebaseDB from "../utils/fire";
 
 const Faq = () => {
   const db = firebaseDB();
+  const mixpanel = useMixpanel();
   const onSubmit = ({ name, email, message }) => {
     const d = new Date();
     const ts = d.toISOString();
-    db.collection("questions").add({ name, email, message, ts });
+    const payload = {
+      name,
+      email,
+      originalMsg: message,
+      ts,
+    };
+    db.collection("contacts").add({
+      ...payload,
+      message: {
+        from: email,
+        text: message,
+        subject: `Message from ${name} at ${ts}`,
+        replyTo: email,
+      },
+      to: ["kristin.stannard@valorumdata.com", "spencer.lyon@valorumdata.com"],
+    });
+    mixpanel.track("Contact Submit", payload);
   };
+
   return (
     <React.Fragment>
       <NavbarThree />
