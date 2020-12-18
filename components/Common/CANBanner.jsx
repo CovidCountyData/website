e803b7a6b4d738568b9519d82ebdad8301c461c4
Spencer Lyon <spencerlyon2@gmail.com>
2020-12-18 14:11:15 -0500

    alternative banner with full width bg

--- components/Common/CANBanner.jsx
+++ components/Common/CANBanner.jsx
@@ -7,45 +7,50 @@ const CANBanner = (props) => {
   const cn = classNames("can-banner", "pt-100", className);
   const mixpanel = useMixpanel();
   return (
-    <div className="container can-banner pt-100">
-      <div className="row justify-content-right">
-        <div className="row can-logo">
-          <div className="col-auto">
-            <img src={require("../../images/can-dark-bg.svg")} alt="CAN Logo" />
-          </div>
-        </div>
+    <div className="can-banner pt-100 justify-content-center d-flex">
+      <div className="can-banner-content">
         <div className="row">
-          <div className="col-auto">
-            <p>
-              After working together for the last seven months, we are joining
-              forces with Covid Act Now (CAN). Our team will now have
-              considerably more resources and support, and we’re excited to
-              expand our reach as we continue to provide you with critical COVID
-              data.
-            </p>
-            <p>What does this mean for you?</p>
+          <div className="row can-logo">
+            <div className="col-auto">
+              <img
+                src={require("../../images/can-dark-bg.svg")}
+                alt="CAN Logo"
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-auto">
+              <p>
+                After working together for the last seven months, we are joining
+                forces with Covid Act Now (CAN). Our team will now have
+                considerably more resources and support, and we’re excited to
+                expand our reach as we continue to provide you with critical
+                COVID data.
+              </p>
+              <p>What does this mean for you?</p>
 
-            <ul>
-              <li>
-                The current Covid County Data (CCD) API will remain available,
-                but any new datasets or expanded data coverage will be
-                integrated solely into the CAN API.
-              </li>
-              <li>We will soon require registration to access our API</li>
-            </ul>
+              <ul>
+                <li>
+                  The current Covid County Data (CCD) API will remain available,
+                  but any new datasets or expanded data coverage will be
+                  integrated solely into the CAN API.
+                </li>
+                <li>We will soon require registration to access our API</li>
+              </ul>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-auto">
-            <div className="can-button">
-              <a
-                id="can-button"
-                onClick={mixpanel.track("visit can button click")}
-                className="btn"
-                href="https://covidactnow.org"
-              >
-                Visit CovidActNow
-              </a>
+          <div className="row">
+            <div className="col-auto">
+              <div className="can-button">
+                <a
+                  id="can-button"
+                  onClick={mixpanel.track("visit can button click")}
+                  className="btn"
+                  href="https://covidactnow.org"
+                >
+                  Visit CovidActNow
+                </a>
+              </div>
             </div>
           </div>
         </div>
